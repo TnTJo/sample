8e9b27d2c440ddd26a00067f553f3fbd36892514
tntmac <tntjoker@163.com>
2019-03-28 03:19:23 +0000

    First update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel - by TnT Joker
                 </div>
 
                 <div class="links">
