6890b1fd6a6674c3bcb4ef82e743763265a14652
tntmac <tntjoker@163.com>
2019-03-28 08:02:58 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,13 +1,18 @@
-<!DOCTYPE html>
+<!doctype html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'TnT App') - The First App of Joker</title>
-</head>
-<body>
-    @yield('content')
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport"
+              content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>@yield('title', 'TnT App') - The First App of Joker</title>
+        <link rel="stylesheet" href="/css/app.css">
+    </head>
+    <body>
+        @include('layouts._header')
+        <div class="container">
+            @yield('content')
+            @include('layouts._footer')
+        </div>
+    </body>
 </html>
